55d4602d1951a083fee4c62f0f9617f5e13babcd
dev.sagartmg <dev.sagartmg@gmail.com>
2025-12-05 09:50:59 +0545

    functions

---
+++ javascript/5-function.js
@@ -0,0 +1,38 @@
+/* calculate sum of two numbers */
+
+/* 
+let number1 = 100;
+let number2 = 200;
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+
+let number3 = 100;
+let number4 = 400;
+console.log(`${number3} + ${number4} = ${number3 + number4}`);
+
+let number5 = 100;
+let number6 = 600;
+console.log(`${number6} + ${number5} = ${number6 + number5}`); 
+*/
+
+function double(number) {
+  console.log(`${number} * 2 = ${number * 2}`);
+}
+
+// double(2);
+// double(4);
+// double(6);
+// double(8);
+
+function sum(number1, number2) {
+  // let number1 = 100 ; let number2 = 200
+  console.log(`${number1} + ${number2} = ${number1 + number2}`);
+}
+
+sum(100, 200);
+sum(200, 200);
+sum(500, 200);
+sum(500, 200, 300, 400);
+
+console.log("end");
+
+console.log("hello", "hi", "welcome");
