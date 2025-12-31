fd64a5585795b20616313a66d1f0da7fa636ad2f
dev.sagartmg <dev.sagartmg@gmail.com>
2025-12-31 09:32:56 +0545

    todo crud using useState

--- my-base-react-project/src/Todo.jsx
+++ my-base-react-project/src/Todo.jsx
@@ -1,37 +1,58 @@
 import { useState } from "react";
 
 export const Todo = () => {
-  const [todos, setTodos] = useState(["one"]);
-  const [showModal, setShowModal] = useState(false);
+  const [todos, setTodos] = useState([
+    { title: "one", status: false },
+    { title: "two", status: false },
+    { title: "three", status: true },
+  ]);
+
+  const [editableTodoIndex, setEditableTodoIndex] = useState(null);
 
   const addNewItem = (e) => {
     e.preventDefault();
     console.log();
     let newTodo = e.target.title.value;
-    let newTodoStatus = e.target.status.checked;
-    console.log(newTodoStatus);
-    console.log({ newTodo });
 
+    // todos.push() // ERROR: cannot change state directly.
     // let newTodos = todos  // ["one" ] // THIS will point to the actual reference of orginal todos variable
     // newTodos.push(newTodo)
     // setTodos(newTodos);
 
     let newTodos = [...todos]; // [one]
-    newTodos.push(newTodo); // [one, two]
+    newTodos.push({ title: newTodo, status: false }); // [one, two]
     setTodos(newTodos);
   };
 
-  const editItem = () => {};
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((el, idx) => index != idx));
+  };
+
+  const editTodo = (index) => {
+    setEditableTodoIndex(index);
+  };
+
+  const editItem = (e) => {
+    e.preventDefault();
+
+    let existingTodos = [...todos];
+    existingTodos[editableTodoIndex].status = e.target.status.checked;
+    existingTodos[editableTodoIndex].title = e.target.title.value;
+
+    setTodos(existingTodos);
+    setEditableTodoIndex(null);
+  };
+
   return (
     <>
       <form className="flex items-center gap-4" onSubmit={addNewItem}>
-        <input type="checkbox" className="text-4xl h-8 w-8" name="status" />
         <input className="border py-4 px-8" placeholder="title" name="title" />
         <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
           submit
         </button>
       </form>
       <br />
+
       <div>{JSON.stringify(todos)}</div>
       <br />
       <table>
@@ -43,17 +64,27 @@ export const Todo = () => {
           </tr>
         </thead>
         <tbody>
-          {todos.map((el) => {
+          {todos.map((el, index) => {
             return (
-              <tr>
-                <td>{el}</td>
-                <td>yes</td>
+              <tr key={index}>
+                <td>{el.title}</td>
+                <td>{el.status ? "Yes" : "No"}</td>
                 <td>
-                  <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
+                  <button
+                    onClick={() => {
+                      editTodo(index);
+                    }}
+                    className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer"
+                  >
                     edit
                   </button>
                   &nbsp;
-                  <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
+                  <button
+                    onClick={() => {
+                      deleteTodo(index);
+                    }}
+                    className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer"
+                  >
                     delete
                   </button>
                 </td>
@@ -63,9 +94,17 @@ export const Todo = () => {
         </tbody>
       </table>
 
-      {showModal && (
-        <div className="  bg-[rgba(0,0,0,0.7)] fixed left-0 right-0 top-0 bottom-0 flex justify-center items-center ">
+      {editableTodoIndex != null && (
+        <div
+          className="  bg-[rgba(0,0,0,0.7)] fixed inset-0 flex justify-center items-center "
+          onClick={() => {
+            setEditableTodoIndex(null);
+          }}
+        >
           <form
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
             className="flex flex-col items-start gap-4 bg-white p-8"
             onSubmit={editItem}
           >
@@ -77,6 +116,8 @@ export const Todo = () => {
                 id="status"
                 className="text-4xl h-8 w-8"
                 name="status"
+                // checked={todos[editableTodoIndex].status}
+                defaultChecked={todos[editableTodoIndex].status}
               />
             </div>
             <div>
@@ -84,6 +125,10 @@ export const Todo = () => {
                 className="border py-4 px-8"
                 placeholder="title"
                 name="title"
+                defaultValue={todos[editableTodoIndex].title}
+                // value={todos[editableTodoIndex].title}
+                // value = "new tiem"
+                // onChange={changeTodo}
               />
             </div>
             <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
